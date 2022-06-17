a39b82d247a83021f8250f84629f48d70f618285
Nguyen Hanh <ndhanh96@gmail.com>
2022-06-17 23:51:44 +0700

    update sidebar layout

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -4,8 +4,8 @@ import React from 'react';
 
 const SideBar = () => {
   return (
-    <div className='hidden md:flex flex-col items-center w-1/5 h-screen bg-blue-200 py-12 gap-12'>
-      <div className='flex items-center justify-center h-32 w-32 rounded-full shadow-xl shadow-blue-500 overflow-hidden bg-blue-400 relative'>
+    <div className='hidden w-1/5 h-screen md:flex flex-col items-center text-sm lg:text-base text-neutral-100 text-shadow-sm bg-cyan-400 brightness-95 py-12 gap-12'>
+      <div className='flex items-center justify-center h-24 w-24 lg:h-24 lg:w-24 rounded-full shadow-xl shadow-blue-800/80 bg-blue-400 overflow-hidden relative'>
         <Image src='/avatar.png' layout='fill' objectFit='cover' />
       </div>
       <div>
