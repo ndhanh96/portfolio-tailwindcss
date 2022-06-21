8c2931d7d1372e634fdaac3d050eb6d2493d034f
Nguyen Hanh <ndhanh96@gmail.com>
2022-06-22 01:36:17 +0700

    add weird fix on layout mobile

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -8,7 +8,7 @@ export const Layout = ({ children }: { children: ReactNode }) => {
       <Head>
         <title>Portfolio | ndhanh96</title>
       </Head>
-      <div className='flex w-full h-screen h-screen-mobile'>
+      <div className='flex w-full absolute inset-0'>
         <SideBar />
         <main className='w-full overflow-auto'>{children}</main>
       </div>
